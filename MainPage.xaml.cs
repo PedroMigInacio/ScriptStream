a83759f0fd4faa3c022a15f0b7c583a56a076aed
Pedro Inacio <pedro.mig.inacio@gmail.com>
2023-03-13 21:12:10 +0000

    fixes grid errors

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,10 +7,30 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
-        for (int i = 0; i < 50; i++)
+
+
+        for (int j = 0; j < 3; j++)
         {
+            var colDefinition = new ColumnDefinition
+            {
+                Width = new GridLength(1, GridUnitType.Star)
+            };
+            gridSystem.ColumnDefinitions.Add(colDefinition);
+        }
+
+            for (int i = 0; i < 50; i++)
+        {
+            var rowDefinition = new RowDefinition
+            {
+                Height = new GridLength(1, GridUnitType.Auto)
+            };
+            gridSystem.RowDefinitions.Add(rowDefinition);
+            //
+            //
             for (int j = 0; j < 3; j++)
             {
+
+
                 Random rnd = new Random();
                 Color randomColor = Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
                 var newColoredBox = new BoxView
@@ -24,7 +44,7 @@ public partial class MainPage : ContentPage
                     Margin = new Thickness(5),
                     VerticalOptions = LayoutOptions.Center,
                     HorizontalOptions = LayoutOptions.Center,
- 
+
                 };
                 gridSystem.Add(newColoredBox, j, i);
                 gridSystem.Add(newLabel, j, i);
